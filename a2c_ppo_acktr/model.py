fix normalization

a2c_ppo_acktr/model.py
@@ -210,7 +210,7 @@ class CNNBase1D(NNBase):
             init_(nn.Conv1d(num_inputs, 32, 8, stride=4)), nn.ReLU(),
             init_(nn.Conv1d(32, 64, 4, stride=2)), nn.ReLU(),
             init_(nn.Conv1d(64, 16, 3, stride=1)), nn.ReLU(), Flatten(),
-            init_(nn.Linear(16 * 41, hidden_size)), nn.ReLU())
+            init_(nn.Linear(16 * 41, hidden_size)), nn.ReLU()) # 16 * 41
 
         init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.
                                constant_(x, 0))

futures_env/environment_cnn.py
@@ -18,6 +18,10 @@ class CNNEnvironment(Environment):
                  window_len=360, min_obs=5, skip_state=True,
                  normalize_feats=False, low=-10, high=10, process_feats=True,
                  random_samp=False, trade_start_secs=int(3600 * 9.5)):
+        self.scaler_features = scaler_features
+        self.diff_features = [f'{feat}_diff' for feat in diff_features]
+        self.all_features = self.diff_features + scaler_features
+        self.normalize_feats = normalize_feats
         super(CNNEnvironment, self).__init__(
             df, diff_features, meta_cols,
             actions=actions,
@@ -25,12 +29,9 @@ class CNNEnvironment(Environment):
             add_features=0,
             process_feats=process_feats,
             random_samp=random_samp,
-            normalize_feats=normalize_feats,
+            normalize_feats=self.normalize_feats,
             skip_state=skip_state
         )
-        self.scaler_features = scaler_features
-        self.diff_features = [f'{feat}_diff' for feat in diff_features]
-        self.all_features = self.diff_features + scaler_features
         self.trade_start_secs = trade_start_secs
         self.observation_space = spaces.Box(
             low=-np.inf, high=np.inf,
@@ -39,12 +40,22 @@ class CNNEnvironment(Environment):
         )
         self.unique_dates = sorted(self.unique_dates)[1:]
         self.window_len = window_len
+        self.mean = 0
 
     def process_features(self):
         df = self.df
         self.df.loc[:, 'secs'] = ((df.time.dt.hour * 3600) + (df.time.dt.minute * 60) + (df.time.dt.second))
         for feat in self.features:
             self.df.loc[:, feat + '_diff'] = self.df[feat].diff()
+        self.df.loc[:, 'last_date'] = self.df.date.shift()
+        self.df.loc[self.df.last_date != self.df.date, self.diff_features] = 0
+
+    def normalize_features(self):
+        df = self.df
+        avg = df[self.all_features].mean().values
+        std = df[self.all_features].std().values
+        self.df.loc[:, self.all_features] -= avg
+        self.df.loc[:, self.all_features] /= std
 
     def reset(self):
         self.cursor = self.min_obs
@@ -61,7 +72,7 @@ class CNNEnvironment(Environment):
         self.cursor = first_index
         self.total_rewards = 0
         # features in each channel are differences. Set them to zero at the start of the day
-        self.day_df.loc[self.day_df.index[self.cursor], self.diff_features] = 0
+        #self.day_df.loc[self.day_df.index[self.cursor], self.diff_features] = self.mean
         state = self.day_df.iloc[self.cursor - self.window_len + 1:self.cursor + 1][self.all_features].T.values.astype(np.float32)
         #print('restate ', state.shape, self.day_df.shape, self.cursor, self.date)
         return state
@@ -78,13 +89,15 @@ class EnvCNNSkipState(CNNEnvironment):
 
     def __init__(self, df=None, set_date=True): # set date is just here for compatibility
         if df is None:
-            df = pd.read_parquet('/Users/brianmcclanahan/git_repos/AllAboutFuturesRL/historical_index_data/S_and_P_historical.parquet')
-            df = df.loc[df.time.between(datetime.datetime(2010, 1, 1), datetime.datetime(2013, 1, 1))]
+            df = pd.read_parquet('/Users/brianmcclanahan/git_repos/pytorch-a2c-ppo-acktr-gail/datasets/S_and_P_train2.parquet')
+            #df = df.loc[df.time.between(datetime.datetime(2010, 1, 1), datetime.datetime(2013, 1, 1))]
         #feature_cols = ['mv_std', 'std_frac', 'sto', 'rsi', 'secs'] 'rsi', 'adx'
         feature_cols = ['close']
+        scaler_features = ['volume']
         meta_cols = ['open', 'high', 'low', 'close', 'date', 'time']
         super(EnvCNNSkipState, self).__init__(
             df, feature_cols, meta_cols, actions=[-10, -5.0, -3,0, -2.0, 0.0, 2.0, 3.0, 5.0, 10.0],
-            min_obs=5, random_samp=False)
+            min_obs=5, random_samp=False, window_len=360,
+            scaler_features=scaler_features, normalize_feats=True)
         if set_date:
             super(EnvCNNSkipState, self).set_date(self.unique_dates[1])